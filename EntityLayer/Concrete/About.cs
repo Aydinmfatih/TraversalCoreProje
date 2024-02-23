5c3606ac854bafe272a20b752f15695234f340e2
aydinmfatih <aydinmfatih@gmail.com>
2024-02-23 22:55:58 +0300

    Entities Add

--- EntityLayer/Concrete/About.cs
+++ EntityLayer/Concrete/About.cs
@@ -9,8 +9,11 @@ namespace EntityLayer.Concrete
     public class About
     {
         public int AboutId { get; set; }
-        public string  Details { get; set; }
+        public string Title { get; set; }
+        public string  Description { get; set; }
         public string  Image { get; set; }
+        public string  Title2 { get; set; }
+        public string  Description2 { get; set; }
         public bool Status { get; set; }
     }
 }
