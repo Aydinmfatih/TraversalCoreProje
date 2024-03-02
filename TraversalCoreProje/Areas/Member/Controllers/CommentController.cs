48bb9bd0bd8b10856691cec9812bbbc335314f3b
aydinmfatih <aydinmfatih@gmail.com>
2024-03-02 19:09:58 +0300

    updated

--- TraversalCoreProje/Areas/Member/Controllers/CommentController.cs
+++ TraversalCoreProje/Areas/Member/Controllers/CommentController.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProje.Areas.Member.Controllers
 {
 	public class CommentController : Controller
 	{
+
+		[Area("Member")]
+		[AllowAnonymous]
 		public IActionResult Index()
 		{
 			return View();
