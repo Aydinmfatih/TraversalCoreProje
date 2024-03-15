f6a86a2af846a66a4f08ef8ff2145dedc1579dd7
aydinmfatih <aydinmfatih@gmail.com>
2024-03-15 18:41:14 +0300

    updated

---
+++ TraversalCoreProje/Areas/Admin/Controllers/DestinationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    public class DestinationController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
